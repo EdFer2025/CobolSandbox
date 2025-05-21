73d5293c474ebc4fcac7ebc817edcaee65e3ce63
Eduardo Feria <eduardo.feria@fau.de>
2025-05-21 23:15:00 +0200

    Refact crun.sh

--- 03_GAME/crun.sh
+++ 03_GAME/crun.sh
@@ -5,4 +5,11 @@ fi
 
 cobc -x 03_GAME/01_main.cob 03_GAME/gameutils.c -o 03_GAME/compiled/01_main
 
-./03_GAME/compiled/01_main
+compile_status=$?
+
+# Check if compilation was successful
+if [ $compile_status -eq 0 ]; then
+    ./03_GAME/compiled/01_main
+else
+    echo "Compilation failed. Game not executed."
+fi
